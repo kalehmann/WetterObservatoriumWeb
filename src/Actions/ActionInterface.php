d945950e544b0e11906ecf9c79cc2a2955140c67
Karsten Lehmann <mail@kalehmann.de>
2021-04-07 00:29:29 +0200

    feat: Interface for an endpoint of the api

---
+++ src/Actions/ActionInterface.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ *  Copyright (C) 2021 Karsten Lehmann <mail@kalehmann.de>
+ *
+ *  This file is part of WetterObservatoriumWeb.
+ *
+ *  WetterObservatoriumWeb is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, version 3 of the License.
+ *
+ *  WetterObservatoriumWeb is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with WetterObservatoriumWeb. If not, see
+ *  <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace KaLehmann\WetterObservatoriumWeb\Actions;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * A interface for a single action (e.g. an endpoint).
+ */
+interface ActionInterface
+{
+    /**
+     * Determines whether the action can handle a request or not.
+     *
+     * This method is used to check whether the path and method of the request
+     * matches to the action or not.
+     *
+     * @param RequestInterface $request is the request in question.
+     *
+     * @return bool whether the action can handle the request or not.
+     */
+    public static function matchesRequest(RequestInterface $request): bool;
+
+    /**
+     * Fulfill the purpose of the action.
+     *
+     * This method receives the request, handles it and return a response.
+     *
+     * @param RequestInterface $request is the request for the action.
+     *
+     * @return ResponseInterface
+     */
+    public function __invoke(RequestInterface $request): ResponseInterface;
+}
