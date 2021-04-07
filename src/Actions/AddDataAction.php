10eb7451b375f5cf8912a4d6fc5aeeffbf8be26b
Karsten Lehmann <mail@kalehmann.de>
2021-04-08 01:06:29 +0200

    feat: Stub action for adding data

---
+++ src/Actions/AddDataAction.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ *  Copyright (C) 2021 Karsten Lehmann <mail@kalehmann.de>
+ *
+ *  This file is part of WetterObservatoriumWeb.
+ *
+ *  WetterObservatoriumWeb is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, version 3 of the License.
+ *
+ *  WetterObservatoriumWeb is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with WetterObservatoriumWeb. If not, see
+ *  <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace KaLehmann\WetterObservatoriumWeb\Actions;
+
+/**
+ * Action for storing weather date.
+ *
+ * This action receives weatehr data from a client (usually an ESP8266),
+ * verifies that the client is allowed to store data and persists it.
+ */
+class AddDataAction implements ActionInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function matchesRequest(RequestInterface $request): bool
+    {
+        if ($request->getMethod() !== 'POST') {
+            return false;
+        }
+
+        return (bool)preg_match('^\/api\/[a-z]+$', $request->getUri());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke(RequestInterface $request): ResponseInterface;
+    {
+
+    }
+}
