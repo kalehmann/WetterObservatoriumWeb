f8b6cdfbdd5b480e5beae4837af9c616e67d06ba
Karsten Lehmann <mail@kalehmann.de>
2021-05-08 01:05:57 +0200

    feat: Ring buffer

---
+++ src/Persistence/RingBuffer.php
@@ -0,0 +1,336 @@
+<?php
+
+/**
+ *  Copyright (C) 2021 Karsten Lehmann <mail@kalehmann.de>
+ *
+ *  This file is part of WetterObservatoriumWeb.
+ *
+ *  WetterObservatoriumWeb is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, version 3 of the License.
+ *
+ *  WetterObservatoriumWeb is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with WetterObservatoriumWeb. If not, see
+ *  <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace KaLehmann\WetterObservatoriumWeb\Persistence;
+
+use \Countable;
+use \IteratorAggregate;
+use \Stringable;
+use \Traversable;
+use function array_filter;
+use function array_keys;
+use function array_key_exists;
+use function array_map;
+use function array_sum;
+use function chr;
+use function count;
+use function explode;
+use function file_get_contents;
+use function join;
+use function pack;
+use function substr;
+use function strlen;
+use function str_repeat;
+use function str_split;
+
+/**
+ * Implementation of a ring buffer with fixed size elements.
+ */
+class RingBuffer implements Countable, IteratorAggregate, Stringable
+{
+    public const HEADER_FORMAT
+        = DataPacker::UNSIGNED_LONG_LE . DataPacker::UNSIGNED_LONG_LE .
+        DataPacker::NULL_BYTE . DataPacker::NULL_BYTE;
+
+    public const HEADER_SIZE
+        = DataPacker::FORMAT_SIZES[DataPacker::UNSIGNED_LONG_LE] +
+        DataPacker::FORMAT_SIZES[DataPacker::UNSIGNED_LONG_LE] +
+        DataPacker::FORMAT_SIZES[DataPacker::NULL_BYTE] +
+        DataPacker::FORMAT_SIZES[DataPacker::NULL_BYTE];
+
+    private int $count;
+
+    private array $data;
+
+    private int $elementSize;
+
+    private string $formatSpec;
+
+    private int $index;
+
+    /**
+     * Creates a ring buffer from existing data.
+     * {@see RingBuffer::createNew} for creating a entirely new ring buffer.
+     *
+     * @param string $contents the packed data of the new ring buffer
+     *                         including the headers.
+     * @param sring $format thu format describing the packed elements.
+     */
+    public function __construct(string $contents, string $format)
+    {
+        $this->elementSize = DataPacker::getElementSize($format);
+        $this->formatSpec = $format;
+
+        [$this->count, $this->index] = $this->readHeader($contents);
+        $this->validateBufferSize($contents);
+        $this->readData($contents);
+    }
+
+    /**
+     * Create a new ring buffer with $numberOfElements elements.
+     *
+     * @param int $numberOfElements the number of elements in the ring buffer.
+     * @param string $format the format describing the packed elements.
+     *                       See the documentation of `pack` for more details.
+     * @return self the new RingBuffer.
+     */
+    public static function createNew(int $numberOfElements, string $format): self
+    {
+        if ($numberOfElements < 1) {
+            throw new IOException(
+                'The number of elements is too small',
+            );
+        }
+
+        if (DataPacker::getFormatElementCount($format) < 1) {
+            throw new IOException(
+                'The format does not contain a single element',
+            );
+        }
+
+        $contents = pack(
+            self::HEADER_FORMAT,
+            $numberOfElements,
+            0,
+        ) . str_repeat(
+            chr(0),
+            $numberOfElements * DataPacker::getElementSize($format),
+        );
+
+        return new self($contents, $format);
+    }
+
+    /**
+     * Load a ring buffer from a existing file.
+     *
+     * @param $path the path to the file with the ring buffer.
+     * @param $format the format describing the packed elements.
+     *                See the documentation of `pack` for more details.
+     * @return self the ring buffer loaded from the file.
+     */
+    public static function fromFile(string $path, string $format): self
+    {
+        $contents = file_get_contents($path);
+        if (false === $contents) {
+            throw new IOException(
+                'Could not read data from ' . $path,
+            );
+        }
+
+        return new self($contents, $format);
+    }
+
+    /**
+     * Open the ring buffer from the specified path and lock it for exclusive
+     * access.
+     *
+     * @param $ptah the path to the ring buffer (must exist already).
+     * @param $format the format describing the packed elements.
+     *                See the documentation of `pack` for more details.
+     * @param $callback a function accpting a {@see RingBuffer::class} as
+     *                  single parameter. All actions on the ring buffer are
+     *                  exclusive without concurrent access from paralell calls
+     *                  to this method.
+     */
+    public static function operateExclusive(
+        string $path,
+        string $format,
+        callable $callback
+    ): void {
+        $stream = fopen($path, 'r+');
+        if (false === $stream) {
+            throw new IOException(
+                'Could not open ' . $path,
+            );
+        }
+
+        if (flock($stream, LOCK_EX)) {
+            $ringBuffer = new self(
+                stream_get_contents($stream),
+                $format,
+            );
+            ftruncate($stream, 0);
+            rewind($stream);
+            $callback($ringBuffer);
+            fwrite($stream, (string)$ringBuffer);
+            fflush($stream);
+            flock($stream, LOCK_UN);
+            fclose($stream);
+
+            return;
+        }
+
+        throw new IOException('Could not aquire lock on ' . $path);
+    }
+
+    /**
+     * Add a new entr to the ring buffer.
+     *
+     * This method overwrites the oldest existing entry.
+     *
+     * @param array $entry the data for the new entry. The number of elements
+     *                     must match the format.
+     */
+    public function addEntry(array $entry): void
+    {
+        $elementCount = DataPacker::getFormatElementCount($this->formatSpec);
+        $entrySize = count($entry);
+        if ($elementCount !== count($entry)) {
+            throw new IOException(
+                'Cannot add an entry with ' . $entrySize . ' elements to the ' .
+                'ring buffer where each entry has a fixed size of ' .
+                $elementCount . '.',
+            );
+        }
+
+        $this->data[$this->index] = array_map(
+            fn ($value): int => (int)$value,
+            $entry,
+        );
+        $this->index = ($this->index + 1) % $this->count;
+    }
+
+    /**
+     * Get the number of entries in the ring buffer.
+     */
+    public function count(): int
+    {
+        return $this->count;
+    }
+
+    /**
+     * Returns an iterator over all elements form the ring buffer in the order
+     * of insertion (the oldest element is the first).
+     */
+    public function getIterator(): Traversable
+    {
+        for ($i = 0; $i < $this->count; $i++) {
+            yield $this->data[($this->index + $i) % $this->count];
+        }
+    }
+
+    /**
+     * Converts the ring buffer to a binary string.
+     */
+    public function __toString(): string
+    {
+        return pack(
+            self::HEADER_FORMAT,
+            $this->count,
+            $this->index,
+        ) . join(
+            '',
+            array_map(
+                fn(array $element) => pack($this->formatSpec, ...$element),
+                $this->data
+            ),
+        );
+    }
+
+    /**
+     * Read the data form $content into the ring buffer.
+     */
+    private function readData(string $content): void
+    {
+        $dataSize = $this->count * $this->elementSize;
+        $data = substr($content, self::HEADER_SIZE, $dataSize);
+        $elements = str_split($data, $this->elementSize);
+        foreach ($elements as $index => $packedEntry) {
+            $unpackedData = $this->unpack($this->formatSpec, $packedEntry);
+            $this->addEntry($unpackedData);
+        }
+    }
+
+    /**
+     * Extracts the header containing the total number of elements and the
+     * index of current element from the data.
+     *
+     * @param string $content is the data of the ring buffer as binary string
+     *
+     * @return array<int> an array of two integers, the first one being the
+     *                    total number of elements in the ring buffer and the
+     *                    second one being the index of the current element.
+     */
+    private function readHeader(string $content): array
+    {
+        $header = substr($content, 0, self::HEADER_SIZE);
+
+        $unpackedHeader = $this->unpack(self::HEADER_FORMAT, $header);
+        [$count, $current] = $unpackedHeader;
+
+        return [$count, $current];
+    }
+
+    /**
+     * Slightly modified version of the built in function `unpack`.
+     *
+     * Same as `unpack` except that it deos not need named elements and returns
+     * a zero indexed array with all unpacked elemets.
+     */
+    private function unpack(string $format, string $packedData): array
+    {
+        $namedFormat = '';
+        for ($i = 0; $i < strlen($format); $i++) {
+            if ($format[$i] === 'x') {
+                $namedFormat .= 'x';
+
+                continue;
+            }
+            $namedFormat .= $format[$i] . '_' . $i . '/';
+        }
+
+        $unpackedData = unpack($namedFormat, $packedData);
+        if (false === $unpackedData) {
+            throw new IOException(
+                'Could not unpack "' . bin2hex($packedData) .
+                '" with format "' . $format . '"',
+            );
+        }
+
+        return array_values(
+            array_filter(
+                $unpackedData,
+                fn ($value, string $key) => $key[0] !== 'd',
+                ARRAY_FILTER_USE_BOTH,
+            )
+        );
+    }
+
+    /**
+     * Checks that $content matches the packed size of the ring buffer.
+     *
+     * @throws IOException if the size of $contents does not match the packed
+     * ring buffer.
+     */
+    private function validateBufferSize(string $content): void
+    {
+        $expectedSize = self::HEADER_SIZE + $this->count * $this->elementSize;
+        $actualSize = strlen($content);
+
+        if ($expectedSize !== $actualSize) {
+            throw new IOException(
+                'The file size of the ring buffer at does not add up.',
+            );
+        }
+    }
+}
---
+++ tests/Persistence/RingBufferTest.php
@@ -0,0 +1,219 @@
+<?php
+
+/**
+ *  Copyright (C) 2021 Karsten Lehmann <mail@kalehmann.de>
+ *
+ *  This file is part of WetterObservatoriumWeb.
+ *
+ *  WetterObservatoriumWeb is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, version 3 of the License.
+ *
+ *  WetterObservatoriumWeb is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with WetterObservatoriumWeb. If not, see
+ *  <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace KaLehmann\WetterObservatoriumWebP\Tests\Persistence;
+
+use KaLehmann\WetterObservatoriumWeb\Persistence\InvalidPackFormatException;
+use KaLehmann\WetterObservatoriumWeb\Persistence\IOException;
+use KaLehmann\WetterObservatoriumWeb\Persistence\RingBuffer;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for the RingBuffer.
+ */
+class RingBufferTest extends TestCase
+{
+    /**
+     * Check that an entry with two elements cannot be added to a ring
+     * buffer with on element per entry.
+     */
+    public function testAddEntryWithInvalidEntry(): void
+    {
+        $ringBuffer = RingBuffer::createNew(4, 'c');
+
+        $this->expectException(IOException::class);
+        $ringBuffer->addEntry([1, 2]);
+    }
+
+    /**
+     * Check that a valid entry can be added to the ring buffer.
+     */
+    public function testAddEntryWithValidEntry(): void
+    {
+        $ringBuffer = RingBuffer::createNew(2, 'l');
+        $ringBuffer->addEntry([31]);
+        $ringBuffer->addEntry([41]);
+
+        $this->assertEquals(
+            [[31], [41]],
+            iterator_to_array($ringBuffer),
+        );
+    }
+
+    /**
+     * Check that more entries than the size of the ring buffer can be added.
+     */
+    public function testAddEntryWithOverflow(): void
+    {
+        $ringBuffer = RingBuffer::createNew(2, 'llll');
+        $ringBuffer->addEntry([1, 2, 3, 4]);
+        $ringBuffer->addEntry([5, 6, 7, 8]);
+        $ringBuffer->addEntry([9, 10, 11, 12]);
+
+        $this->assertEquals(
+            [[5, 6, 7, 8], [9, 10, 11, 12]],
+            iterator_to_array($ringBuffer),
+        );
+    }
+
+    /**
+     * Check, that the number of elements in the ring buffer can be determined
+     * with `count`.
+     */
+    public function testCount(): void
+    {
+        $ringBuffer = RingBuffer::createNew(20, 'N');
+        $this->assertEquals(
+            20,
+            count($ringBuffer),
+        );
+    }
+
+    /**
+     * Check that a new ring buffer can be created.
+     */
+    public function testCreateNew(): void
+    {
+        $ringBuffer = RingBuffer::createNew(6, 'qq');
+        $this->assertEquals(6, count($ringBuffer));
+    }
+
+    /**
+     *
+     */
+    public function testCreateNewWithInvalidFormat(): void
+    {
+        $this->expectException(IOException::class);
+        $this->expectExceptionMessage('The format does not contain a single element');
+        RingBuffer::createNew(40, 'xx');
+    }
+
+    /**
+     * Check that creating a ring buffer with an invalid format throws an
+     * exception.
+     */
+    public function testCreateNewWithFormatWithoutElements(): void
+    {
+        $this->expectException(InvalidPackFormatException::class);
+        RingBuffer::createNew(10, 'invalid');
+    }
+
+    /**
+     * Check that creating a ring buffer with less than one element throws
+     * an exception.
+     */
+    public function testCreateNewWithInvalidNumberOfElements(): void
+    {
+        $this->expectException(IOException::class);
+        $this->expectExceptionMessage('The number of elements is too small');
+        RingBuffer::createNew(0, 'qcc');
+    }
+
+    /**
+     * Check that the ring buffer can be iterated.
+     */
+    public function testIteration(): void
+    {
+        $ringBuffer = RingBuffer::createNew(4, 'q');
+        $this->assertEquals(
+            [[0], [0], [0], [0]],
+            iterator_to_array($ringBuffer),
+        );
+
+        $ringBuffer->addEntry([1]);
+        $ringBuffer->addEntry([2]);
+        $ringBuffer->addEntry([3]);
+        $ringBuffer->addEntry([4]);
+        $this->assertEquals(
+            [[1], [2], [3], [4]],
+            iterator_to_array($ringBuffer),
+        );
+    }
+
+    /**
+     * Check that an exclusive operation on the ring buffer is possible.
+     */
+    public function testOperateExclusive(): void
+    {
+        $tempFile = tempnam(
+            sys_get_temp_dir(),
+            'testOperateExclusive',
+        );
+
+        try {
+            $ringBuffer = RingBuffer::createNew(4, 'qq');
+            file_put_contents($tempFile, (string)$ringBuffer);
+
+            RingBuffer::operateExclusive(
+                $tempFile,
+                'qq',
+                function (RingBuffer $ringBuffer): void {
+                    $ringBuffer->addEntry([1, 2]);
+                    $ringBuffer->addEntry([3, 4]);
+                    $ringBuffer->addEntry([5, 6]);
+                    $ringBuffer->addEntry([7, 8]);
+
+                    $this->assertEquals(
+                        [[1, 2], [3, 4], [5, 6], [7, 8]],
+                        iterator_to_array($ringBuffer),
+                    );
+                },
+            );
+
+            RingBuffer::operateExclusive(
+                $tempFile,
+                'qq',
+                function (RingBuffer $ringBuffer): void {
+                    $ringBuffer->addEntry([9, 10]);
+
+                    $this->assertEquals(
+                        [[3, 4], [5, 6], [7, 8], [9, 10]],
+                        iterator_to_array($ringBuffer),
+                    );
+                },
+            );
+        } finally {
+            unlink($tempFile);
+        }
+    }
+
+    /**
+     * Check that a ring buffer can be converted to a string and then be read
+     * again.
+     */
+    public function testToString(): void
+    {
+        $ringBuffer = RingBuffer::createNew(4, 'lcxxx');
+        $ringBuffer->addEntry([1, 11]);
+        $ringBuffer->addEntry([2, 22]);
+        $ringBuffer->addEntry([3, 33]);
+        $ringBuffer->addEntry([4, 44]);
+
+        $data = (string)$ringBuffer;
+        $ringBuffer2 = new RingBuffer($data, 'lcxxx');
+        $this->assertEquals(
+            [[1, 11], [2, 22], [3, 33], [4, 44]],
+            iterator_to_array($ringBuffer2),
+        );
+    }
+}
