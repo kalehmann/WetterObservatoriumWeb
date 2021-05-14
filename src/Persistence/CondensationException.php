2ac90b5e9af0877768631c62cfef84ba5972c0bb
Karsten Lehmann <mail@kalehmann.de>
2021-05-14 23:49:40 +0200

    feat: CondensationException

---
+++ src/Persistence/CondensationException.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ *  Copyright (C) 2021 Karsten Lehmann <mail@kalehmann.de>
+ *
+ *  This file is part of WetterObservatoriumWeb.
+ *
+ *  WetterObservatoriumWeb is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, version 3 of the License.
+ *
+ *  WetterObservatoriumWeb is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with WetterObservatoriumWeb. If not, see
+ *  <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace KaLehmann\WetterObservatoriumWeb\Persistence;
+
+use \Exception;
+
+/**
+ * Exception for anything related to the condensation of data.
+ */
+class CondensationException extends Exception
+{
+}
